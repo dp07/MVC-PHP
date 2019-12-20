277442f5a19276359fe263e696d6576134e0992d
dp07 <dp.madridiztazc@gmail.com>
2019-12-20 22:53:05 +0700

    update

---
+++ app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php 
+class About {
+	public function index($nama = 'Dany Pradana' , $pekerjaan = 'Tukang Tidur')
+ 	{
+ 		echo "Halo nama saya $nama, saya adalah seorang $pekerjaan";
+ 	}
+
+ 	public function page()
+ 	{
+ 		echo 'About/page';
+ 	}
+}
+?>
