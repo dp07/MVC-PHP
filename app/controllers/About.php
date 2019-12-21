70d4ff9c4e31e40a813c6d31cd9b112156552406
dp07 <dp.madridiztazc@gmail.com>
2019-12-21 10:45:06 +0700

    update

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,13 +1,22 @@
 <?php 
-class About {
-	public function index($nama = 'Dany Pradana' , $pekerjaan = 'Tukang Tidur')
+class About extends Controller {
+	public function index($nama = 'Dany Pradana' , $pekerjaan = 'Tukang Tidur', $umur = 20)
  	{
- 		echo "Halo nama saya $nama, saya adalah seorang $pekerjaan";
+ 		$data['nama'] = $nama;
+ 		$data['pekerjaan'] = $pekerjaan;
+ 		$data['umur'] = $umur;
+ 		$data['judul'] = 'About';
+ 		$this->view('templates/header', $data);
+ 		$this->view('about/index', $data);
+ 		$this->view('templates/footer');
  	}
 
  	public function page()
  	{
- 		echo 'About/page';
+ 		$data['judul'] = 'Page';
+ 		$this->view('templates/header', $data);
+ 		$this->view('about/page');
+ 		$this->view('templates/footer');
  	}
 }
 ?>
---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container mt-3">
+	<h1>About me</h1>
+	<img src="<?=BASEURL; ?>/img/dp.png" alt="Dany Pradana" width="200" class="rounded-circle shadow">
+	<p>Nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
